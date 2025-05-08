016b587057105600577bb1402a0088840c8a7684
willymariino <wg.mariino@gmail.com>
2025-05-08 16:18:31 +0200

    configurazione database

--- data/db.js
+++ data/db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'pizzeria'
+    password: 'root',
+    database: 'blog_db'
 });
 
 // connettiamoci
